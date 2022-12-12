b621213d2cdfb0c828d79f83c4cd23e3f46b9c3b
Andrey Skripachev <andrey.skripachev@saritasa.com>
2022-12-12 11:11:15 +0700

    Add functional for buyers

--- src/renderer/features/buyers/index.tsx
+++ src/renderer/features/buyers/index.tsx
@@ -10,14 +10,49 @@ const BuyersTableComponent: FC = () => {
   const [buyers, setBuyers] = useState<readonly Buyer[]>([]);
   const [removable, setRemovable] = useState<null | number>(null);
   const [editable, setEditable] = useState<Buyer | null>(null);
+  const [newBuyer, setNewBuyer] = useState<Omit<Buyer, 'id'> | null>(null);
 
   // eslint-disable-next-line promise/catch-or-return
   BuyerService.get().then(setBuyers);
 
+  const handleEdit = (key: keyof Buyer, value: Buyer[typeof key]) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    editable &&
+      setEditable({
+        ...editable,
+        [key]: value,
+      });
+  };
+
+  const handleAddNewBuyer = (key: keyof Buyer, value: Buyer[typeof key]) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    newBuyer &&
+      setNewBuyer({
+        ...newBuyer,
+        [key]: value,
+      });
+  };
+
   return (
     <>
       <table className={style.table}>
-        <caption className={style.caption}>Buyers</caption>
+        <caption className={style.caption}>
+          Buyers
+          <button
+            type="button"
+            className={style.add}
+            onClick={() =>
+              setNewBuyer({
+                address: '',
+                email: '',
+                name: '',
+                phone: '',
+              })
+            }
+          >
+            +
+          </button>
+        </caption>
         <thead>
           <tr>
             <th>ID</th>
@@ -70,12 +105,120 @@ const BuyersTableComponent: FC = () => {
       {editable && (
         <Modal>
           <div className={style.editInfo}>Edit menu</div>
+          <div>
+            <div className={style.editField}>
+              <span>Name</span>
+              <input
+                type="text"
+                value={editable.name}
+                onChange={({ target: { value } }) => {
+                  handleEdit('name', value);
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Email</span>
+              <input
+                type="text"
+                value={editable.email}
+                onChange={({ target: { value } }) => {
+                  handleEdit('email', value);
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Phone</span>
+              <input
+                type="text"
+                value={editable.phone}
+                onChange={({ target: { value } }) => {
+                  handleEdit('phone', value);
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Address</span>
+              <input
+                type="text"
+                value={editable.address}
+                onChange={({ target: { value } }) => {
+                  handleEdit('address', value);
+                }}
+              />
+            </div>
+          </div>
           <div className={style.editActions}>
             <button type="button" onClick={() => setEditable(null)}>
               Cancel
             </button>
             <button type="button" onClick={() => {}}>
               Save
+            </button>
+          </div>
+        </Modal>
+      )}
+      {newBuyer && (
+        <Modal>
+          <p className={style.modalTitle}>Add new buyer</p>
+          <div>
+            <div className={style.editField}>
+              <span>Name</span>
+              <input
+                type="text"
+                value={newBuyer.name}
+                onChange={({ target: { value } }) => {
+                  handleAddNewBuyer('name', value.trim());
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Email</span>
+              <input
+                type="text"
+                value={newBuyer.email}
+                onChange={({ target: { value } }) => {
+                  handleAddNewBuyer('email', value.trim());
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Phone</span>
+              <input
+                type="text"
+                value={newBuyer.phone}
+                onChange={({ target: { value } }) => {
+                  handleAddNewBuyer('phone', value.trim());
+                }}
+              />
+            </div>
+            <div className={style.editField}>
+              <span>Address</span>
+              <input
+                type="text"
+                value={newBuyer.address}
+                onChange={({ target: { value } }) => {
+                  handleAddNewBuyer('address', value.trim());
+                }}
+              />
+            </div>
+          </div>
+          <div className={style.editActions}>
+            <button
+              type="button"
+              className={style.edit}
+              onClick={() => {
+                BuyerService.post(newBuyer);
+                setNewBuyer(null);
+              }}
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className={style.cancel}
+              onClick={() => setNewBuyer(null)}
+            >
+              Cancel
             </button>
           </div>
         </Modal>
